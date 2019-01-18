a3ac906d2bb9bfcebfff95a268c6589a761044ea
Andreas Amzoll <aamzoll@Sirius.saturnfacilities.com>
2019-01-18 13:56:54 +0100

    added struct

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+struct Tile
+{
+	int id;			// ID of tile
+	int type;		// Type of tile, e.g. water, sand, grass, stone, etc.
+	int posX;		// Position of tile in x direction
+	int posY;		// Position of tile in y direction
+	bool pass;	// passability of tile, true or false
+};
+
 int main()
 {
 	std::cout << "Hallo Welt!" << std::endl;
